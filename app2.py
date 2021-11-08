de98e38702c323aa0f43836f957cfc03ce461783
unknown <i.penju1022@gmail.com>
2021-11-08 15:35:20 +0900

    app2

--- app2.py
+++ app2.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 
+def disLabel():
+    lbl.configure(text="こんにちわ")
+
 root = tk.Tk()
 root.geometry("200x100")
 
 lbl = tk.Label(text="LABEL")
-btn = tk. Button(text="PUSH")
+btn = tk. Button(text="PUSH", command = dispLabel)
 
 lbl.pack()
 btn.pack()
